1d8e60242beea46fd76938e9a8ed760d60cc0113
Your Name <danamonuraalhaji@gmail.com>
2025-10-03 08:22:36 +0100

    commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,14 +3,17 @@
 const Home =()=>{
     return(
         <div className="body">
+            <div className="home2">
+             <h3>Welcome to Attasfiyah Portal</h3>
                     <div className='home'>
-                        <h4>Welcome to Attasfiyah Portal</h4>
+                       
                         <div> 
                                 <input type="text" placeholder="Enter username"/><br />
                                 <input type="password" placeholder="Password" /> <br />
                            <div style={{display:"grid",justifyItems:"center"}}> <button>Sign In</button>  </div>
                         </div>
                     </div>
+            </div>
         </div>
     )
 }
