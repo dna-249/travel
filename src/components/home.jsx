0e5a19aee66ea1f27124593d3b691f732d0b4c39
Your Name <danamonuraalhaji@gmail.com>
2025-10-03 08:31:22 +0100

    commit

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,7 @@ const Home =()=>{
             <div className="home2">
              <h3>Welcome to Attasfiyah Portal</h3>
                     <div className='home'>
-                       
+                       <p style={{fontSize:"15px",color:" rgb(48, 46, 46)",textAlign:"center"}}> Sign in to Continue</p>
                         <div> 
                                 <input type="text" placeholder="Enter username"/><br />
                                 <input type="password" placeholder="Password" /> <br />
