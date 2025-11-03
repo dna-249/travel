c9233ad673988d856e130fef7f25f1393f534c98
nuraalhaji <you@example.com>
2025-11-03 12:48:51 +0100

    commit

--- src/components/reportPage.jsx
+++ src/components/reportPage.jsx
@@ -405,7 +405,7 @@ const calculateOverallData = (subjects) => {
                     <img src="https://placehold.co/70x70/004D40/FFFFFF?text=Logo" alt="School Logo" className="logo" />
                     <div className="school-info">
                         <p style={{fontSize:"20px"}} className="school-name-ar">مدرسة التصفية للتحفيظ والدراسات الإسلامية، أبوجا</p>
-                        <h2 className="school-name-en">MADRASAT ALTASFIYAH TAHFEEZ AND ISLAMIYAH SCHOOL, ABUJA</h2>
+                        <h2 className="school-name-en"  onClick={()=>{console.log(rawStudentData); console.log(MOCK_DATA)}}>MADRASAT ALTASFIYAH TAHFEEZ AND ISLAMIYAH SCHOOL, ABUJA</h2>
                         <h3 className="report-title">STATEMENT OF RESULT</h3>
                     </div>
                     <img src="https://placehold.co/70x70/E0F2F1/333333?text=Photo" alt="Student" className="student-photo" />
