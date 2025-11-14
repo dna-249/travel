c4d387afb356cfbe43950e5c6b9246b6472966dc
nuraalhaji <you@example.com>
2025-11-14 09:25:51 +0100

    commit

--- src/components/admin.jsx
+++ src/components/admin.jsx
@@ -221,7 +221,7 @@ const AdminDashboard = () => {
                             {students.map(student => (
                                 <option key={student._id} value={student._id}>
                                     {/* Display Student Name and ID/Class for easy lookup */}
-                                    {`${student.studentName} (ID: ${student._id})`}
+                                    {`${student.studentName}`}
                                 </option>
                             ))}
                         </select>
