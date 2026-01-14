1bba080c07782c1c18bd5f1ca7d07ebd1c1c882f
Your Name <danamonuraalhaji@gmail.com>
2026-01-14 21:06:52 +0100

    commi

--- src/components/prograss.jsx
+++ src/components/prograss.jsx
@@ -30,7 +30,7 @@ const WeeklyReport = () => {
     setStatus(prev => ({ ...prev, [section]: 'loading' }));
     
     try {
-      const response = await axios.put(`https://portal-database-seven.vercel.app/student/push/${id}`, {
+      const response = await axios.put(`https://portal-database-seven.vercel.app/student/push/69086a3eeb4adef8689d1948`, {
        
 date:teacherData.date,
 tajweed:teacherData.tajweed,
