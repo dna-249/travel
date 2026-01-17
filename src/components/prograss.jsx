03379ea1bfc3f02f61426912c7e0d6ed3a2740f8
Your Name <danamonuraalhaji@gmail.com>
2026-01-17 20:27:23 +0100

    commi

--- src/components/prograss.jsx
+++ src/components/prograss.jsx
@@ -184,7 +184,7 @@ const WeeklyReport = () => {
                     />
                   </td>
                 ))}
-                <td onClick={()=>setDay(row.day)} className={day === row.day? " border border-black text-white bg-sky-500/100 font-bold text-center": "border border-black bg-gray-50 font-bold text-center"}>{row.day}</td>
+                <td  className= "border border-black bg-gray-50 font-bold text-center">{row.day}</td>
               </tr>
             ))}
           </tbody>
