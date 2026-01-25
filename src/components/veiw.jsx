6b6276650ee1917ea40b0c4fc24c51d54c5b355e
Your Name <danamonuraalhaji@gmail.com>
2026-01-25 18:36:07 +0100

    hhhh

--- src/components/veiw.jsx
+++ src/components/veiw.jsx
@@ -266,12 +266,13 @@ const createDataSource = (daily,k) => {
       <section className="mb-10 border-b-2 border-gray-300 pb-8">
         <h1 className="text-2xl font-bold text-center text-blue-900 mb-4 tracking-tighter">TEACHER'S WEEKLY REPORT</h1>
         
-        <div className="flex justify-between mb-4 font-bold text-[12px]">
-          <div>Week <input className={underlineInput} value={teacherComments.week} onChange={e => setTeacherComments({...teacherComments, week: e.target.value})} /> الأسبوع</div>
-          <div style={{ paddingRight:"50px"}} onClick={()=>back()}><FaArrowAltCircleLeft/> </div>
-          <div style={{ paddingRight:"50px"}} onClick={()=>move()}><FaArrowAltCircleRight /></div>
-          <div>Term <input className={underlineInput} value={teacherComments.term} onChange={e => setTeacherComments({...teacherComments, term: e.target.value})} /> الفترة</div>
-        </div>
+         <div className="flex justify-between mb-4 font-bold text-[12px]">
+                  <div>Week <input className={underlineInput}value={!teacherComments.week ? response?.weeks?.[increase]?.weeks: teacherComments.week} onChange={e => setTeacherComments({...teacherComments, week: e.target.value})} /> الأسبوع</div>
+                  <div style={{ paddingRight:"50px"}} onClick={()=>back()}><FaArrowAltCircleLeft/> </div>
+                  <div style={{ paddingRight:"50px"}} onClick={()=>move()}><FaArrowAltCircleRight /></div>
+                  <div>Term <input className={underlineInput} value={!teacherComments.term ? response?.terms?.[increase]?.terms: teacherComments.term} onChange={e => setTeacherComments({...teacherComments, term: e.target.value})} /> الفترة</div>
+                </div>
+        
 
         <table className="w-full border-collapse border border-black mb-4">
           <thead className="bg-gray-100 uppercase text-[9px]">
